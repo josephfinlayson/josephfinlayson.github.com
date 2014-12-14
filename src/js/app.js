b49d81267f8e262abc226f88c6966e41754d3040
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-12-14 00:10:50 +0000

    tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,39 @@
 'use strict';
 
 angular.module('x', ['ui.router', 'dd.fake-typer'])
-  .run(function($state, $timeout){
+    .run(function($state, $timeout) {
 
-    $timeout(function(){
-      if (window.Modernizr && !window.Modernizr.flexbox){
-        $state.go('me');
-      }
-    }, 0);
+        $timeout(function() {
+            if (window.Modernizr && !window.Modernizr.flexbox) {
+                $state.go('me');
+            }
+        }, 0);
 
-  })
-  .config(function ($stateProvider, $urlRouterProvider) {
+    })
+    .config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('me');
+        $urlRouterProvider.otherwise('me');
 
-    $stateProvider
-      .state('me', {
-        url: '/me',
-        templateUrl: 'views/me.html',
-        contoller: 'meCtrl as meCtrl'
-      })
-      .state('contact', {
-        url: '/contact',
-        templateUrl: 'views/contact.html'
-      })
-      .state('achievements', {
-        url: '/achievements',
-        templateUrl: 'views/achievements.html'
-      })
-      .state('rocket', {
-        url: '/rocket',
-        templateUrl: 'views/rocket.html'
-      })
-    ;
+        $stateProvider
+            .state('me', {
+                url: '/me',
+                templateUrl: 'views/me.html',
+                contoller: 'meCtrl as meCtrl'
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'views/contact.html'
+            })
+            .state('achievements', {
+                url: '/achievements',
+                templateUrl: 'views/achievements.html'
+            })
+            .state('rocket', {
+                url: '/rocket',
+                templateUrl: 'views/rocket.html'
+            });
 
 
-  })
+    })
 
 ;
